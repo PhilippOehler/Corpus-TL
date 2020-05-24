ab864b87ac52e5c740d12248469d491d9e74e87f
root <root@LAPTOP-JKV672Q4.localdomain>
2020-05-24 20:35:01 +0200

    s010x development

--- src/sg01procCorpus/sg01procCorpus.sh
+++ src/sg01procCorpus/sg01procCorpus.sh
@@ -1,1 +1,1 @@
-python sg01procCorpus.py ../../data/s02vert-txt/gnu_en_de_terms_v04__english > gnu_en__de_v04__english-extract.txt
+python3 sg01procCorpus.py ../../data/s02vert-text/gnu_en_de_terms_v04__english.vert  > gnu_en__de_v04__english-extract.txt
